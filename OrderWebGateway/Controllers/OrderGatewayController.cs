d324e9d7579bb630ac956dd01890134968eaf333
Matthew Thomas <Matthew.Thomas690@googlemail.com>
2019-11-16 20:38:01 +0000

    initial commit

---
+++ OrderWebGateway/Controllers/OrderGatewayController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OrderWebGateway.Models;
+
+namespace OrderWebGateway.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderGatewayController : ControllerBase
+    {
+        private readonly ILogger<OrderGatewayController> logger;
+
+        [HttpPost]
+        public void Post(OrderModel orderModel)
+        {
+
+        }
+    }
+}
---
+++ OrderWebGateway/Models/OrderModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderWebGateway.Models
+{
+    public class OrderModel
+    {
+        public string OrderId { get; set; }
+
+        public Dictionary<string, object> ProductPayloads { get; set; } = new Dictionary<string, object>();
+
+    }
+}
